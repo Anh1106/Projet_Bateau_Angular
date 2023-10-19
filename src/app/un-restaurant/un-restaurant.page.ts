0345131cd7e598a59f5a00e3c76613a2eafdb383
nguyenngocanh <ngocanhnguyen11601@gmail.com>
2023-10-19 22:55:12 +0200

    Update Image

--- src/app/un-restaurant/un-restaurant.page.ts
+++ src/app/un-restaurant/un-restaurant.page.ts
@@ -9,6 +9,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class UnRestaurantPage implements OnInit {
   title : any;
   infos : any;
+  img : any;
 
   constructor(private route: ActivatedRoute, private router: Router) {
     this.route.queryParamMap.subscribe((params) => {
@@ -16,6 +17,7 @@ export class UnRestaurantPage implements OnInit {
       if (navigationExtras && navigationExtras.state) {
         this.infos = navigationExtras['state']['info'];
         this.title = navigationExtras['state']['title'];
+        this.img = navigationExtras['state']['img'];
       }
     });
   }
--- src/app/une-recette/une-recette.page.ts
+++ src/app/une-recette/une-recette.page.ts
@@ -9,6 +9,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class UneRecettePage implements OnInit {
   title : any;
   infos : any;
+  img : any;
 
   constructor(private route: ActivatedRoute, private router: Router) {
     this.route.queryParamMap.subscribe((params) => {
@@ -16,6 +17,7 @@ export class UneRecettePage implements OnInit {
       if (navigationExtras && navigationExtras.state) {
         this.infos = navigationExtras['state']['info'];
         this.title = navigationExtras['state']['title'];
+        this.img = navigationExtras['state']['img'];
       }
     });
   }
