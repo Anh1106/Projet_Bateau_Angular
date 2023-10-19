77cb7d62a1c192368223b7796325ac074e19e2a9
nguyenngocanh <ngocanhnguyen11601@gmail.com>
2023-10-19 18:26:02 +0200

    Add Resto

--- src/app/restaurants/restaurants.page.ts
+++ src/app/restaurants/restaurants.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-restaurants',
@@ -7,14 +7,37 @@ import { Router } from '@angular/router';
   styleUrls: ['./restaurants.page.scss'],
 })
 export class RestaurantsPage implements OnInit {
+  menu = [
+    {
+      title: 'Bistrot Landais',
+      info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis enim non lacinia ultricies. Vivamus libero nisl, dapibus nec ex vitae, laoreet ullamcorper purus. Etiam tincidunt, velit a rhoncus sodales, libero orci iaculis elit, quis accumsan erat justo sit amet justo. Pellentesque vel scelerisque augue. Integer eros arcu, facilisis vitae sem a, tempor vehicula eros. Maecenas dignissim eget mauris eu auctor. Phasellus pulvinar, diam at fringilla efficitur, nibh purus bibendum diam, at tempus magna eros et ligula. Maecenas placerat luctus purus et vestibulum. Cras quis libero erat. Donec in tellus ex. Curabitur scelerisque eros sit amet tempus interdum. Vestibulum nec eros neque.",
+    },
+    {
+      title: 'Bistrot Landais',
+      info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis enim non lacinia ultricies. Vivamus libero nisl, dapibus nec ex vitae, laoreet ullamcorper purus. Etiam tincidunt, velit a rhoncus sodales, libero orci iaculis elit, quis accumsan erat justo sit amet justo. Pellentesque vel scelerisque augue. Integer eros arcu, facilisis vitae sem a, tempor vehicula eros. Maecenas dignissim eget mauris eu auctor. Phasellus pulvinar, diam at fringilla efficitur, nibh purus bibendum diam, at tempus magna eros et ligula. Maecenas placerat luctus purus et vestibulum. Cras quis libero erat. Donec in tellus ex. Curabitur scelerisque eros sit amet tempus interdum. Vestibulum nec eros neque.",
+    },
+    {
+      title: 'Bistrot des Gascons',
+      info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis enim non lacinia ultricies. Vivamus libero nisl, dapibus nec ex vitae, laoreet ullamcorper purus. Etiam tincidunt, velit a rhoncus sodales, libero orci iaculis elit, quis accumsan erat justo sit amet justo. Pellentesque vel scelerisque augue. Integer eros arcu, facilisis vitae sem a, tempor vehicula eros. Maecenas dignissim eget mauris eu auctor. Phasellus pulvinar, diam at fringilla efficitur, nibh purus bibendum diam, at tempus magna eros et ligula. Maecenas placerat luctus purus et vestibulum. Cras quis libero erat. Donec in tellus ex. Curabitur scelerisque eros sit amet tempus interdum. Vestibulum nec eros neque.",
 
-  constructor(private router: Router) { }
+    },
+    {
+      title: 'Les fous de lîle',
+      info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis enim non lacinia ultricies. Vivamus libero nisl, dapibus nec ex vitae, laoreet ullamcorper purus. Etiam tincidunt, velit a rhoncus sodales, libero orci iaculis elit, quis accumsan erat justo sit amet justo. Pellentesque vel scelerisque augue. Integer eros arcu, facilisis vitae sem a, tempor vehicula eros. Maecenas dignissim eget mauris eu auctor. Phasellus pulvinar, diam at fringilla efficitur, nibh purus bibendum diam, at tempus magna eros et ligula. Maecenas placerat luctus purus et vestibulum. Cras quis libero erat. Donec in tellus ex. Curabitur scelerisque eros sit amet tempus interdum. Vestibulum nec eros neque."
+    },
+  ];
 
-  onGoToUnRestaurant(){
-    this.router.navigate(['/un-restaurant']);
+  goToResto(title: any,info : any) {
+    let navigationExtras: NavigationExtras = {
+      state: {
+        title: title,
+        info: info,
+      },
+    };
+    this.router.navigate(['/un-restaurant'], navigationExtras);
   }
 
-  ngOnInit() {
-  }
+  constructor(private router: Router) {}
 
+  ngOnInit() {}
 }
--- src/app/un-restaurant/un-restaurant.page.ts
+++ src/app/un-restaurant/un-restaurant.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-un-restaurant',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./un-restaurant.page.scss'],
 })
 export class UnRestaurantPage implements OnInit {
+  title : any;
+  infos : any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.route.queryParamMap.subscribe((params) => {
+      const navigationExtras = this.router.getCurrentNavigation()?.extras;
+      if (navigationExtras && navigationExtras.state) {
+        this.infos = navigationExtras['state']['info'];
+        this.title = navigationExtras['state']['title'];
+      }
+    });
   }
 
+  ngOnInit() {}
 }
+
