eb90da8fb159c53715aaae4180474a9a1c89ea7b
Quang Minh PHAM <quangminh1575@gmail.com>
2023-10-20 10:57:55 +0200

    update

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,22 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.page.html',
   styleUrls: ['./cart.page.scss'],
 })
 export class CartPage implements OnInit {
-  purchasedPlats: Array<{ name: string; price: number; purchase: boolean;quantity:number }> = [];
-
-  constructor() { }
+  purchasedPlats: Array<{
+    name: string;
+    price: number;
+    purchase: boolean;
+    quantity: number;
+  }> = [];
+  totalPrice: number = 0;
+  constructor() {}
 
   ngOnInit() {
     const purchasedPlatsStr = localStorage.getItem('purchasedPlats');
     if (purchasedPlatsStr) {
       this.purchasedPlats = JSON.parse(purchasedPlatsStr);
+      this.calculateTotalPrice();
     }
 
     console.log(this.purchasedPlats);
+  }
+
+  calculateTotalPrice() {
+    this.totalPrice = this.purchasedPlats.reduce(
+      (total, plat) => total + plat.price * plat.quantity,
+      0
+    );
   }
 }
